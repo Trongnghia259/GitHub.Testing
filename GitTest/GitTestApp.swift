1baf0e7d8a2e865e4b790b22be72bd1142d9f5c7
Trongnghia259 <67889697+Trongnghia259@users.noreply.github.com>
2023-10-24 08:31:05 +0700

    New Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Tam Nguyen on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
