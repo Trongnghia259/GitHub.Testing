18c04e19e556097d0dde8aea7a36a0256d881ed6
Trongnghia259 <67889697+Trongnghia259@users.noreply.github.com>
2023-10-24 09:25:01 +0700

    New Commit

--- GitTest/ContentView.swift
+++ GitTest/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Image(systemName: "home.fill")
+                .imageScale(.large)
+                .foregroundStyle(.blue)
         }
         .padding()
     }
